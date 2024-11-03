450a5ceca63c86c2e928ac23268edec85c8880a5
itdan-com <107674489+itdan-com@users.noreply.github.com>
2024-11-03 01:00:09 -0700

    Update script.js
    State Grouping

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
+// Create an object to store the state groups
+var stateGroups = {};
+
 // Load GeoJSON data
 fetch('data/base-locations.geojson')
   .then(response => {
@@ -15,22 +18,43 @@ fetch('data/base-locations.geojson')
     return response.json();
   })
   .then(data => {
-    // Add GeoJSON data to the map with interaction
+    // Add GeoJSON data to the map with interaction and grouping by state
     L.geoJSON(data, {
       onEachFeature: function (feature, layer) {
         // Check if feature properties exist
         if (feature.properties) {
+          var state = feature.properties.stateNameCode;
+          
           // Create a popup with relevant feature properties
           var popupContent = `
             <strong>Site Name:</strong> ${feature.properties.siteName}<br>
             <strong>Feature Name:</strong> ${feature.properties.featureName}<br>
             <strong>Operational Status:</strong> ${feature.properties.siteOperationalStatus}<br>
-            <strong>State:</strong> ${feature.properties.stateNameCode}<br>
+            <strong>State:</strong> ${state}<br>
             <strong>FIRRMA Site:</strong> ${feature.properties.isFirrmaSite}
           `;
           layer.bindPopup(popupContent);
+
+          // Add the layer to the appropriate state group
+          if (!stateGroups[state]) {
+            stateGroups[state] = L.layerGroup().addTo(map);
+          }
+          stateGroups[state].addLayer(layer);
         }
       }
     }).addTo(map);
+
+    // Create a control to show state group counts
+    var stateCountControl = L.control({ position: 'topright' });
+    stateCountControl.onAdd = function () {
+      var div = L.DomUtil.create('div', 'info legend');
+      div.innerHTML = '<h4>State Site Counts</h4>';
+      Object.keys(stateGroups).forEach(state => {
+        var count = stateGroups[state].getLayers().length;
+        div.innerHTML += `<strong>${state}:</strong> ${count} sites<br>`;
+      });
+      return div;
+    };
+    stateCountControl.addTo(map);
   })
   .catch(err => console.error('Error loading GeoJSON:', err));
