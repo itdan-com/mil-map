c7458ab30b76ba6de33fdaefdf29449d8fcea303
itdan-com <107674489+itdan-com@users.noreply.github.com>
2024-11-03 01:12:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+<!-- Include Leaflet MarkerCluster library -->
+<link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster/dist/MarkerCluster.css" />
+<link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster/dist/MarkerCluster.Default.css" />
+<script src="https://unpkg.com/leaflet.markercluster/dist/leaflet.markercluster.js"></script>
+
+<script>
 // Create the map and set the initial view
 var map = L.map('map').setView([37.0902, -95.7129], 5); // Center coordinates for the U.S. with a zoom level of 5
 
@@ -6,8 +12,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Create an object to store the state groups
-var stateGroups = {};
+// Create a marker cluster group
+var markers = L.markerClusterGroup();
 
 // Load GeoJSON data
 fetch('data/base-locations.geojson')
@@ -18,43 +24,28 @@ fetch('data/base-locations.geojson')
     return response.json();
   })
   .then(data => {
-    // Add GeoJSON data to the map with interaction and grouping by state
+    // Add GeoJSON data to the marker cluster group with interaction
     L.geoJSON(data, {
       onEachFeature: function (feature, layer) {
         // Check if feature properties exist
         if (feature.properties) {
-          var state = feature.properties.stateNameCode;
-          
           // Create a popup with relevant feature properties
           var popupContent = `
             <strong>Site Name:</strong> ${feature.properties.siteName}<br>
             <strong>Feature Name:</strong> ${feature.properties.featureName}<br>
             <strong>Operational Status:</strong> ${feature.properties.siteOperationalStatus}<br>
-            <strong>State:</strong> ${state}<br>
+            <strong>State:</strong> ${feature.properties.stateNameCode}<br>
             <strong>FIRRMA Site:</strong> ${feature.properties.isFirrmaSite}
           `;
           layer.bindPopup(popupContent);
-
-          // Add the layer to the appropriate state group
-          if (!stateGroups[state]) {
-            stateGroups[state] = L.layerGroup().addTo(map);
-          }
-          stateGroups[state].addLayer(layer);
         }
+        // Add the layer to the cluster group
+        markers.addLayer(layer);
       }
-    }).addTo(map);
+    });
 
-    // Create a control to show state group counts
-    var stateCountControl = L.control({ position: 'topright' });
-    stateCountControl.onAdd = function () {
-      var div = L.DomUtil.create('div', 'info legend');
-      div.innerHTML = '<h4>State Site Counts</h4>';
-      Object.keys(stateGroups).forEach(state => {
-        var count = stateGroups[state].getLayers().length;
-        div.innerHTML += `<strong>${state}:</strong> ${count} sites<br>`;
-      });
-      return div;
-    };
-    stateCountControl.addTo(map);
+    // Add the marker cluster group to the map
+    map.addLayer(markers);
   })
   .catch(err => console.error('Error loading GeoJSON:', err));
+</script>
